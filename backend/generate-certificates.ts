804bfe7426d151b7558882a9bb6ff580f9779d69
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-11-11 20:54:54 +0200

    CircleCI + SonarQube

--- backend/generate-certificates.ts
+++ backend/generate-certificates.ts
@@ -5,6 +5,6 @@ import path from "path";
 if (!existsSync(path.join(process.cwd(), "..", "certs")))
   mkdirSync(path.join(process.cwd(), "..", "certs"));
 
-await $`openssl genpkey -algorithm RSA -out ../certs/server.key -aes256`;
+await $`openssl genpkey -algorithm RSA -out ../certs/server.key -aes256 -passout pass:${process.env.KEY_PASSPHRASE}`;
 
-await $`openssl req -new -x509 -key ../certs/server.key -out ../certs/server.crt -days 365`;
+await $`openssl req -new -x509 -key ../certs/server.key -out ../certs/server.crt -days 365 -passin pass:${process.env.KEY_PASSPHRASE}`;
